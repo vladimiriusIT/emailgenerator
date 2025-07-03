2d88240b722cfb8d8b0738c3af7fb3b0b89bba1c
Vladimir Stratiev <vladimiriusIT@users.noreply.github.com>
2025-07-03 14:16:06 +0300

    Initial Commit

---
+++ src/main/java/people/spheres/emailgenerator/EmailgeneratorApplication.java
@@ -0,0 +1,13 @@
+package people.spheres.emailgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailgeneratorApplication.class, args);
+	}
+
+}
